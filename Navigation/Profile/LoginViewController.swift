f293adeb036758304dae37b1bc871edeb3b7a7e3
Rus <ruzaki.dev@gmail.com>
2026-04-18 14:14:33 +0300

    Added user authentication and profile data flow

--- Navigation/Profile/LoginViewController.swift
+++ Navigation/Profile/LoginViewController.swift
@@ -9,6 +9,9 @@ final class LoginViewController: UIViewController {
     
     // MARK: Visual content
     
+    // Dependancy
+    private var userService: UserService!
+    
     var loginScrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.translatesAutoresizingMaskIntoConstraints = false
@@ -97,6 +100,15 @@ final class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let user = User(
+            login: "HipsterCat",
+            fullName: "Hipster Cat",
+            avatar: UIImage(named: "hipster_cat") ?? UIImage(),
+            status: "Waiting for something..."
+        )
+        
+        userService = CurrentUserService(user: user)
+        
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.isHidden = true
         
@@ -166,10 +178,34 @@ final class LoginViewController: UIViewController {
 
     }
     
+    private func showAlert(message: String) {
+        let alert = UIAlertController(
+            title: "Ошибка",
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+    
     // MARK: - Event handlers
 
     @objc private func touchLoginButton() {
+        
+        guard let login = loginField.text, !login.isEmpty else {
+            showAlert(message: "Введите логин")
+            return
+        }
+        
+        guard let user = userService.checkUser(login: login) else {
+            showAlert(message: "Неверный логин")
+            return
+        }
+        
         let profileVC = ProfileViewController()
+        profileVC.user = user
+        
         navigationController?.setViewControllers([profileVC], animated: true)
     }
 
