9e386eb42ce160991730ebc34ae83ef999b606a8
Rus <ruzaki.dev@gmail.com>
2026-04-25 14:02:46 +0300

    refactor: move UserService logic to protocol extension

--- Navigation/Services/UserService.swift
+++ Navigation/Services/UserService.swift
@@ -2,7 +2,15 @@
 //  UserService.swift
 //  Navigation
 //
+import Foundation
 
 protocol UserService {
+    var user: User { get set }
     func checkUser(login: String) -> User?
+}
+
+extension UserService {
+    func checkUser(login: String) -> User? {
+        return login == user.login ? user : nil
+    }
 }
