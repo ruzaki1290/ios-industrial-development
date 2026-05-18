367b98538af1c70c4e5c5da7d19e5b573f54f9fe
Rus <ruzaki.dev@gmail.com>
2026-05-18 21:52:11 +0300

    Move app launch flow to AppCoordinator

--- Navigation/Coordinators/AppCoordinator.swift
+++ Navigation/Coordinators/AppCoordinator.swift
@@ -2,6 +2,46 @@
 //  AppCoordinator.swift
 //  Navigation
 //
-//  Created by Rus Zakirov on 16.05.2026.
-//
+
+import UIKit
+
+final class AppCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    
+    private let window: UIWindow
+    private let tabBarController = UITabBarController()
+    
+    init(window: UIWindow, navigationController: UINavigationController = UINavigationController()) {
+        self.window = window
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        
+        // create tab bar with feed and profile items
+        let loginVC = LoginViewController()
+        let profileNC = UINavigationController(rootViewController: loginVC)
+        profileNC.tabBarItem = UITabBarItem(title: "Profile",
+                                            image: UIImage(systemName: "person.crop.circle"),
+                                            selectedImage: UIImage(systemName: "person.crop.circle.fill")
+        )
+        
+        let feedVC = FeedViewController()
+        let feedNC = UINavigationController(rootViewController: feedVC)
+        feedNC.tabBarItem = UITabBarItem(title: "Feed",
+                                         image: UIImage(systemName: "text.bubble"),
+                                         selectedImage: UIImage(systemName: "text.bubble.fill"))
+
+        tabBarController.tabBar.backgroundColor = .white
+        tabBarController.viewControllers = [profileNC, feedNC]
+        
+        // activate main window
+        window.rootViewController = tabBarController
+        window.makeKeyAndVisible()
+        
+    }
+    
+    
+} // AppCoordinator
 
