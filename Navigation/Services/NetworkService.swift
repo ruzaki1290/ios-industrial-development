02ac3e1ef28dbb23e9feeb43119b1af8e3a86a09
Rus <ruzaki.dev@gmail.com>
2026-06-03 11:40:33 +0300

    Implement URLSession request handling

--- Navigation/Services/NetworkService.swift
+++ Navigation/Services/NetworkService.swift
@@ -21,12 +21,40 @@ struct NetworkService {
         
         let session = URLSession.shared
         
-        let task = session.dataTask(with: url) {
-            data, response, error in
+        let task = session.dataTask(with: url) { data, response, error in
+            
+            // При использовании swapi.dev был получен:
+            // Error Code: -1200
+            // "A TLS error caused the secure connection to fail."
+            //
+            // При отключении интернета был получен:
+            // Error Code: -1001
+            // "The request timed out."
+            
+            if let error {
+                print("Ошибка: \(error.localizedDescription)")
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                
+                print("Status Code: \(httpResponse.statusCode)")
+                
+                print("Headers:")
+                print(httpResponse.allHeaderFields)
+                
+            }
+            
+            if let data,
+               let stringData = String(data: data, encoding: .utf8) {
+                print("Data:")
+                print(stringData)
+            }
+            
         }
         
         task.resume()
         
-    }
+    } // request
     
 } // NetworkService
