f4e600963dcb521173ae974d83bb71a7553bee44
Rus <ruzaki.dev@gmail.com>
2026-06-05 19:31:50 +0300

    Add JSONSerialization and Decodable parsing

--- Navigation/InfoViewController.swift
+++ Navigation/InfoViewController.swift
@@ -18,6 +18,17 @@ final class InfoViewController: UIViewController {
         
     }()
     
+    private let planetOrbitalLabel: UILabel = {
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.text = "Planet loading..."
+        return label
+        
+    }()
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -27,14 +38,22 @@ final class InfoViewController: UIViewController {
         createAlertButton()
         
         view.addSubview(todoTitleLabel)
-        
+        view.addSubview(planetOrbitalLabel)
+
         NSLayoutConstraint.activate([
+            
             todoTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             todoTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            todoTitleLabel.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: -50)
+            todoTitleLabel.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: -120),
+            
+            planetOrbitalLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            planetOrbitalLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            planetOrbitalLabel.topAnchor.constraint(equalTo: todoTitleLabel.bottomAnchor, constant: 20)
+            
         ])
         
         fetchTodo()
+        fetchPlanet()
         
     }
     
@@ -73,9 +92,41 @@ final class InfoViewController: UIViewController {
         }
 
         task.resume()
-    }
+        
+    } // fetchTodo()
     
-    
+    private func fetchPlanet() {
+        
+        guard let url = URL(string: "https://swapi.info/api/planets/1") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                let planet = try JSONDecoder().decode(Planet.self, from: data)
+                
+                DispatchQueue.main.async {
+                    self?.planetOrbitalLabel.text = "Tatooine orbital period: \(planet.orbitalPeriod)"
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+        task.resume()
+        
+    } // fetchPlanet()
+        
     private func createAlertButton() {
         
         let button = CustomButton(title: "Alert") { [weak self] in
@@ -92,8 +143,6 @@ final class InfoViewController: UIViewController {
         ])
         
     }
-    
-   
     
     @objc func tapAlertButton() {
         let alert = UIAlertController(title: "Attention",
@@ -114,3 +163,13 @@ final class InfoViewController: UIViewController {
     }
     
 } // InfoViewController
+
+
+private struct Planet: Decodable {
+    let orbitalPeriod: String
+    
+    enum CodingKeys: String, CodingKey {
+        case orbitalPeriod = "orbital_period"
+    }
+    
+}
