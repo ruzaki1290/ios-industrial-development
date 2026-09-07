d84e21a96d8b212309d35390d6379935404fc10e
Rus <ruzaki.dev@gmail.com>
2026-09-07 11:45:29 -0400

    Initialized Firebase on app launch

--- Navigation/AppDelegate.swift
+++ Navigation/AppDelegate.swift
@@ -4,6 +4,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -12,8 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var appCoordinator: AppCoordinator?
     
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions:
+        [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        FirebaseApp.configure()
         
         window = UIWindow(frame: UIScreen.main.bounds)
         
